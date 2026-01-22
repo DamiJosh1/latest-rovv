d58c4142e5195d618a3f52c97d7413787c71986a
Dami-Josh <124748465+DamiJosh1@users.noreply.github.com>
2026-01-22 04:54:48 +0100

    Update SafetySection.tsx

--- src/components/pages/get-to-know-rovv/SafetySection.tsx
+++ src/components/pages/get-to-know-rovv/SafetySection.tsx
@@ -109,7 +109,7 @@ export default function SafetySection() {
                 onClick={() => setIsModalOpen(false)}
                 className="mt-6 text-primary font-bold text-sm text-left hover:underline"
               >
-                Close
+                
               </button>
             </motion.div>
           </motion.div>
@@ -117,4 +117,5 @@ export default function SafetySection() {
       </AnimatePresence>
     </section>
   )
+
 }
