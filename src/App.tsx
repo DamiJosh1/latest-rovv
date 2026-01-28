cfa6e6226ead0a7104ee54ce576aeba738310a2a
Dami-Josh <124748465+DamiJosh1@users.noreply.github.com>
2026-01-28 20:26:11 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
 import {Routes, Route} from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import { AnimatePresence } from 'framer-motion'
-
-
-
-import Preloader from './components/preloader/Preloader.tsx'
 
 import HomePage from './components/pages/index.tsx'
 import GetToKnowRovvPage from './components/pages/get-to-know-rovv/index.tsx'
@@ -22,32 +16,9 @@ import TermsAndCondition from './components/pages/terms&condition/index.tsx'
 
 
 function App () {
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    // Hidden timer
-    const handleLoad = () => {
-      setTimeout(() => setLoading(false), 2500)
-    }
-
-    if (document.readyState === 'complete') {
-      handleLoad()
-    }
-    else{
-      window.addEventListener('load', handleLoad)
-    }
-
-    return () => window.removeEventListener('load', handleLoad)
-  },[])
-
-
-
-  return (
+  
+return (
     <>
-
-      <AnimatePresence mode="wait">
-        {loading && <Preloader />}
-      </AnimatePresence>
 
     <Routes>
       <Route path='/' element={<HomePage />} />
